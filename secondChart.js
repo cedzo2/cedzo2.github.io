ab441acc7fab1c0f611efa891b3e69e65c7c0555
cedzo2 <89861256+cedzo2@users.noreply.github.com>
2023-07-29 22:15:58 -0400

    Update secondChart.js

--- secondChart.js
+++ secondChart.js
@@ -2,7 +2,7 @@ let data;
 
 function createSecondChart() {
   d3.select("#chart2").selectAll("svg").remove();
-  const dataUrl = "https://github.com/cedzo2/MyFiles/blob/main/Pokemon_Dataset.csv";
+  const dataUrl = "/Pokemon_Dataset.csv";
 
   const margin = { top: 30, right: 80, bottom: 50, left: 50 };
   const width = 600 - margin.left - margin.right;
